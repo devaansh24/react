c20dc58b1ba142cbce56cd9f3137013cd6c719db
Devansh Sharma <devanshsharma@Devanshs-MacBook-Air.local>
2023-04-30 11:29:12 +0530

    prod build initiated

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am child1"),
-    React.createElement("h2", {}, "I am child2"),
-    React.createElement("h1", {}, "I am child1"),
-    React.createElement("h2", {}, "I am child2"),
-  ])
+    "div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am Devansh"),
+        React.createElement("h2", {}, "I am child2"),
+        React.createElement("h1", {}, "I am child1"),
+        React.createElement("h2", {}, "I am child2"),
+    ])
 );
 
 console.log(heading);
