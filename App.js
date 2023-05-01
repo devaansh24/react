b3b7fe9f487bd0ed1b2a504cf51faf74d4846863
Devansh Sharma <devanshsharma@Devanshs-MacBook-Air.local>
2023-05-01 17:52:24 +0530

    front-end 1 working

--- App.js
+++ App.js
@@ -1,16 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-const heading = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I am Devansh"),
-        React.createElement("h2", {}, "I am child2"),
-        React.createElement("h1", {}, "I am child1"),
-        React.createElement("h2", {}, "I am child2"),
-    ])
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import resList from "./utils/mockData";
+import { CDN_URL } from "./utils/constants";
+/**
+ * Header
+ *  -Logo
+ *  -Nav Item
+ * Body
+ *  -Search
+ *  -Restaurant Container
+ *  -Image
+ *  Name of Res,rating,Cuisines,Delivery time,offers
+ * Footer
+ *  -Copyright
+ *  -Links
+ *  -Address
+ *  -Contact
+ */
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://img.freepik.com/premium-vector/simple-minimalist-food-bag-restaurant-delivery-service-logo-design-vector_493994-1029.jpg"
+          alt="app-logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-console.log(heading);
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const {
+    cloudinaryImageId,
+    name,
+    avgRating,
+    cuisines,
+    costForTwo,
+    deliveryTime,
+  } = resData?.data;
+  return (
+    <>
+      <div className="res-card">
+        <img className="res-logo" src={CDN_URL + cloudinaryImageId} />
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{deliveryTime} mins</h4>
+        <h4>{costForTwo / 100}</h4>
+      </div>
+    </>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<AppLayout />);
